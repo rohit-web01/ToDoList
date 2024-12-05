7b0c09a7cab9346eeda48fea66b1e3f24ed0436c
Rohit Gehlot <rohitgehlot5621@gmail.com>
2024-12-06 02:40:34 +0530

    Adding Local Storage Feature & Changing Background

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let btn = document.querySelector("button");
 let input = document.querySelector("#input-box");
 let taskList = document.querySelector(".task-container");
 
+// Adding Input Task to Task List
 btn.addEventListener("click", () => {
     if(input.value === ''){
         alert("You must write something!");
@@ -14,12 +15,28 @@ btn.addEventListener("click", () => {
         crossBtn.innerHTML = "\u00d7";
         task.appendChild(crossBtn);
     }
+    saveData();
 });
 
+// Adding & Removing Task That Are Completed
 taskList.addEventListener("click", (evt) => {
     if(evt.target.tagName === "LI"){
         evt.target.classList.toggle("checked");
+        saveData();
     }else if(evt.target.tagName === "SPAN"){
         evt.target.parentNode.remove();
+        saveData();
     }
 });
+
+// For Saving Data to Local Storage.
+function saveData(){
+    localStorage.setItem("data", taskList.innerHTML);
+}
+
+// For Showing Previous After Reload
+function showTask(){
+    taskList.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
